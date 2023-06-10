81b99b1485a71995d43bf7863c0cbe668e714214
Nikita Jagdale <128221436+NikitaJagdale88@users.noreply.github.com>
2023-06-10 23:19:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,8 +19,10 @@ const library = [
 const numberOfBooksRead = () => {
   // write your code here
 	let count = 0;
-	for(let i=0; i<library.length; i++){
-		if(library[i].readingStatus == true){
+	for(let i = 0; i < library.length; i++) 
+	{
+		if(library[i].readingStatus == true)
+		{
 			count++;
 		}
 	}
